a3ea745c4eabd4756ddb3bd5acc95c4fc9486a2a
matthisRipoche <matthisripoche@gmail.com>
2024-02-27 16:29:45 +0100

    ok

--- main.js
+++ main.js
@@ -1,3 +1,17 @@
 const timer = setInterval(function () {
   //   Code
+  const dateActuelle = new Date();
+  const dateFinAnnee = new Date("Jan 1 00:00:00 2025");
+
+  const timer = document.getElementById("timer");
+
+  const tempsRestant = new Date((dateFinAnnee - dateActuelle) / 1000);
+
+  console.log(dateActuelle);
+  console.log(tempsRestant);
+
+  if(tempsRestant > 0){
+    timer.innerHTML = "";
+    timer.innerHTML += tempsRestant;
+  }
 }, 1000);
