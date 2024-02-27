c43d6ad0cd482ced72faa07566a70d0c0baef659
matthisRipoche <matthisripoche@gmail.com>
2024-02-27 16:40:16 +0100

    ok

--- main.js
+++ main.js
@@ -5,13 +5,20 @@ const timer = setInterval(function () {
 
   const timer = document.getElementById("timer");
 
-  const tempsRestant = new Date((dateFinAnnee - dateActuelle) / 1000);
-
-  console.log(dateActuelle);
-  console.log(tempsRestant);
+  const tempsRestant = new Date((dateFinAnnee - dateActuelle));
 
   if(tempsRestant > 0){
+    const jours = tempsRestant.getUTCDate() - 1; // Soustraire 1 pour obtenir le nombre de jours correct
+    const mois = tempsRestant.getUTCMonth();
+    const annees = tempsRestant.getUTCFullYear() - 1970; // Soustraire 1970 pour obtenir le nombre d'années correct
+    const heures = tempsRestant.getUTCHours();
+    const minutes = tempsRestant.getUTCMinutes();
+    const secondes = tempsRestant.getUTCSeconds();
+
     timer.innerHTML = "";
-    timer.innerHTML += tempsRestant;
+    timer.innerHTML += `${annees}a ${mois}m ${jours}j ${heures}h ${minutes}min ${secondes}sec`;
+  } else{
+    timer.innerHTML = "";
+    timer.innerHTML = "Bonne année";
   }
 }, 1000);
